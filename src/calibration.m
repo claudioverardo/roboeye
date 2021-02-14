657721fcb3aff70e6ff6f899923b810cb1277eb1
mattiabalutto <mattia.balutto@gmail.com>
2021-02-14 01:33:30 +0100

    Try to run the calibration script

--- src/calibration.m
+++ src/calibration.m
@@ -1,6 +1,6 @@
 % Calculate P and K matrices of the two cameras
-[P1, K1] = calibration_camera('../assets/calibration/01', 19);
-[P2, K2] = calibration_camera('../assets/calibration/02', 19);
+[P1, K1] = calibration_camera('../assets/calibration/01', 16);
+[P2, K2] = calibration_camera('../assets/calibration/02', 16);
 
 % Calculate stereo calibration paramenters
 [F, E, deltaT] = calibration_stereo(P1, P2);
