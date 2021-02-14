549c7907fb7085576e7b9c332a6c231ed6f17c40
claver <claudioverardo@gmail.com>
2021-02-14 12:38:19 +0100

    New check_cvx_quadrilateral with matrix expressions

--- src/m-files/check_cvx_quadrilateral.m
+++ src/m-files/check_cvx_quadrilateral.m
@@ -1,38 +1,71 @@
-function is_cvx_quad = check_cvx_quadrilateral(points)
+function is_cvx_quad = check_cvx_quadrilateral(points, varargin)
+    % points contain the vertices of a 2D figure
+    % points = [ [x1,y1]; [x2,y2]; ...; [xN,yN] ]
     
-    % points = [ [x1,y1]; [x2,y2]; [x3,y3]; [x4,y4] ] vertices
+    % Default values of parameters
+    default_sum_angles_tolerance  = 10; % degrees
+    default_parallelism_tolerance = 15; % degrees
+    
+    % Input parser
+    p = inputParser;
+    addParameter(p, 'sum_angles_tolerance', default_sum_angles_tolerance);
+    addParameter(p, 'parallelism_tolerance', default_parallelism_tolerance);
+    parse(p, varargin{:});
+    
+    % Parse function parameters
+    SUM_ANGLES_TOLERANCE  = p.Results.sum_angles_tolerance;
+    PARALLELISM_TOLERANCE = p.Results.parallelism_tolerance;
     
     is_cvx_quad = 0;
-    vectors = zeros(4,2);
-    angles = zeros(4,1);
     
+    % Check if the points define a quadrilateral
     if size(points,1) == 4
-        for i=1:4
-            vectors(i,:) = points(mod(i,4)+1,:) - points(i,:);
-        end
         
-        for i=1:4
-            angles(i) = pi - acos(...
-                vectors(mod(i,4)+1,:) * vectors(i,:)' ...
-                ./ norm(vectors(mod(i,4)+1,:)) ./ norm( vectors(i,:)) ...
-            );
-        end
+        % Calculate the vectors from each vertex to the next one
+        % and put them in a matrix arranged by column
+        V = (circshift(points,-1,1) - points)';
+        V_normalized = V ./ vecnorm(V);
         
-        tot_angle = sum(angles);
+        % Calculate the internal angles of the quadrilateral
+        angles = pi - acos( dot(circshift(V_normalized,-1,2), V_normalized) );
         
-        if tot_angle > 0.97*2*pi && tot_angle < 1.03*2*pi
+        % Calculate the total internal angle of the quadrilateral
+        sum_angles = sum(angles);
+        sum_angles_degree = sum_angles / pi * 180;
+        
+        % Check if the points define a convex quadrilateral
+        if sum_angles_degree > 360-SUM_ANGLES_TOLERANCE && ...
+           sum_angles_degree < 360+SUM_ANGLES_TOLERANCE
             
-            parallel_sides_angles = zeros(2,1);
-            for i=1:2
-                parallel_sides_angles(i) = acos(...
-                    abs( vectors(i+2,:) * vectors(i,:)' ) ...
-                    ./ norm(vectors(i+2,:)) ./ norm( vectors(i,:)) ...
-                ); 
-            end
-                
-            if parallel_sides_angles(1) < pi/10 && parallel_sides_angles(2) < pi/10
+            % Calculate the angles between opposite sides
+            parallel_sides_angles = acos( abs( dot(circshift(V_normalized,-2,2), V_normalized) ) );
+            parallel_sides_angles_degree = parallel_sides_angles ./ pi * 180;
+            
+            % Check if the points define a quasi-parallelogram
+            if parallel_sides_angles_degree(1) < PARALLELISM_TOLERANCE && ...
+               parallel_sides_angles_degree(2) < PARALLELISM_TOLERANCE
                 is_cvx_quad = 1;
             end
+            
+            % OLD IMPLEMENTATION, JUST AS BACKUP
+            % V_old = zeros(4,2);
+            % angles_old = zeros(4,1);
+            % parallel_sides_angles_old = zeros(2,1);
+            % for i=1:4
+            %     V_old(i,:) = points(mod(i,4)+1,:) - points(i,:);
+            % end
+            % for i=1:4
+            %     angles_old(i) = pi - acos(...
+            %         V_old(mod(i,4)+1,:) * V_old(i,:)' ...
+            %         ./ norm(V_old(mod(i,4)+1,:)) ./ norm( V_old(i,:)) ...
+            %     );
+            % end
+            % for i=1:2
+            %     parallel_sides_angles_old(i) = acos(...
+            %         abs( V_old(i+2,:) * V_old(i,:)' ) ...
+            %         ./ norm(V_old(i+2,:)) ./ norm( V_old(i,:)) ...
+            %     ); 
+            % end
             
         end
         
