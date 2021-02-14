f4f1a8d8f276cca62d22b65a25286d834b739f7d
mattiabalutto <mattia.balutto@gmail.com>
2021-02-14 21:42:24 +0100

    Continues trying on calibration the two cameras

--- src/calibration.m
+++ src/calibration.m
@@ -1,6 +1,6 @@
 % Calculate P and K matrices of the two cameras
-[P1, K1] = calibration_camera('../assets/calibration/01', 16);
-[P2, K2] = calibration_camera('../assets/calibration/02', 16);
+% [P1, K1] = calibration_camera('../assets/calibration/01', 15);
+[P2, K2] = calibration_camera('../assets/calibration/02', 15);
 
 % Calculate stereo calibration paramenters
 [F, E, deltaT] = calibration_stereo(P1, P2);
