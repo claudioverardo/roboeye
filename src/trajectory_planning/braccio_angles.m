db037367e493d7e495141ce1a752f5bf1f598be6
DiegoPerissutti <diegoperissutti97@gmail.com>
2021-03-11 21:19:15 +0100

    Add convertion to braccio's angles convention

---
+++ src/trajectory_planning/braccio_angles.m
@@ -0,0 +1,6 @@
+#### convert angles from model convention to robot conv 
+function out=braccio_angles(in)
+
+out=in+[90 90 90 90 180];
+
+end
---
+++ src/trajectory_planning/print_for_arduino.m
@@ -0,0 +1,18 @@
+function done=print_for_arduino(Q,numtraj)
+  %PRINT ON A TXT FILE TRAJECTORY ARRAY TO PASTE INTO ARDUINO SCRIPT
+  
+  [npoints,~]=size(Q);
+  
+  directory='';
+  
+  file = fopen(strcat(directory,sprintf('Arduino_trajectory_%d.txt',numtraj)),'w');
+  
+  for i=1:npoints
+     printpart=round(Q(i,[2 3 4 5 6 7]));
+     fprintf(file,'{%d, %d, %d, %d, %d, %d},\n',printpart);
+  end
+  fclose(file);
+  
+  done=1;
+  
+endfunction
