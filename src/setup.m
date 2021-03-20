75f6235711f0e0f9a97a5346946ea6d92b8535a5
mattiabalutto <mattia.balutto@gmail.com>
2021-03-20 21:17:43 +0100

    Remove addpath data from the setup file

--- src/setup.m
+++ src/setup.m
@@ -3,7 +3,6 @@
 
 addpath(fullfile(pwd, 'vision'));
 addpath(fullfile(pwd, 'calibration'));
-addpath(fullfile(pwd, 'data'));
 addpath(fullfile(pwd, 'thirdparty'));
 addpath(fullfile(pwd, 'scripts'));
 
