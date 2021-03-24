db7f60531a850800e537e912ddb053a008cebfbf
claudioverardo <claudioverardo@gmail.com>
2021-03-25 00:46:24 +0100

    Add tests with Aruco 8x8

---
+++ assets/img_tests/README.md
@@ -0,0 +1,4 @@
+Images to test Aruco detection and Aruco pose estimation.
+
++ The white box has dimensions 15.5 x 11.5 x 7.6 cm.
++ The brown boxes have dimensions 12 x 11.5 x 6 cm.
--- src/scripts/run_pose_estimation.m
+++ src/scripts/run_pose_estimation.m
@@ -1,30 +1,65 @@
 close all; % clear
 
+%% TESTS - POSE ESTIMATION - ARUCO MARKERS 7x7
+%--------------------------------------------------------------------------
+
 config_file = '../assets/config_files/config_pose_estimation';
 K_file = '../assets/calibration/intrinsics_cam1/K.mat';
-R_cam_file = '../assets/calibration/extrinsics_cam1_tests/R_cam.mat';
-t_cam_file = '../assets/calibration/extrinsics_cam1_tests/t_cam.mat';
+R_cam_file = '../assets/calibration/extrinsics_cam1_tests_7x7/R_cam.mat';
+t_cam_file = '../assets/calibration/extrinsics_cam1_tests_7x7/t_cam.mat';
+aruco_markers_file = '../assets/aruco_markers/aruco_markers_7x7.mat';
+aruco_real_sides = [3 3 3 3 4 4 5 5]; % [cm]
 
-% img_source = webcam(1);
-% img_source = '../assets/img_tests/7x7/img_7x7_04_01.png';
+img_source = '../assets/img_tests/7x7/img_7x7_04_01.png';
 % img_source = '../assets/img_tests/7x7/img_7x7_04_02.png';
 % img_source = '../assets/img_tests/7x7/img_7x7_04_03.png';
 % img_source = '../assets/img_tests/7x7/img_7x7_06_01.png';
 % img_source = '../assets/img_tests/7x7/img_7x7_06_02.png';
-img_source = '../assets/img_tests/7x7/img_7x7_06_03.png';
+% img_source = '../assets/img_tests/7x7/img_7x7_06_03.png';
 % img_source = '../assets/img_tests/7x7/img_7x7_08_01.png';
 % img_source = '../assets/img_tests/7x7/img_7x7_08_02.png';
 % img_source = '../assets/img_tests/7x7/img_7x7_12_01.png';
 % img_source = '../assets/img_tests/7x7/img_7x7_12_02.png';
 
-aruco_markers_file = '../assets/aruco_markers/aruco_markers_7x7.mat';
-aruco_real_sides = [3 3 3 3 4 4 5 5]; % [cm]
 
+%% TESTS - POSE ESTIMATION - ARUCO MARKERS 8x8
+%--------------------------------------------------------------------------
+
+% config_file = '../assets/config_files/config_pose_estimation';
+% K_file = '../assets/calibration/intrinsics_cam1/K.mat';
+% R_cam_file = '../assets/calibration/extrinsics_cam1_tests_8x8/R_cam.mat';
+% t_cam_file = '../assets/calibration/extrinsics_cam1_tests_8x8/t_cam.mat';
 % aruco_markers_file = '../assets/aruco_markers/aruco_markers_8x8.mat';
 % aruco_real_sides = [3 3 3 4 4 6]; % [cm]
 
+% img_source = '../assets/img_tests/8x8/img_8x8_04_01.png';
+% img_source = '../assets/img_tests/8x8/img_8x8_04_02.png';
+% img_source = '../assets/img_tests/8x8/img_8x8_04_03.png';
+% img_source = '../assets/img_tests/8x8/img_8x8_04_04.png';
+% img_source = '../assets/img_tests/8x8/img_8x8_06_01.png';
+% img_source = '../assets/img_tests/8x8/img_8x8_06_02.png';
+% img_source = '../assets/img_tests/8x8/img_8x8_06_03.png';
+% img_source = '../assets/img_tests/8x8/img_8x8_06_04.png';
+% img_source = '../assets/img_tests/8x8/img_8x8_08_01.png';
+% img_source = '../assets/img_tests/8x8/img_8x8_08_02.png';
+% img_source = '../assets/img_tests/8x8/img_8x8_08_03.png';
 
-%% -----------------------------------------------------------
+
+%% TESTS - POSE ESTIMATION (NEW)
+%--------------------------------------------------------------------------
+
+% config_file = 'path/to/config/file.m';
+% K_file = 'path/to/intrinsics/K.mat';
+% R_cam_file = 'path/to/extrinsics/R.mat';
+% t_cam_file = 'path/to/extrinsics/t.mat';
+% aruco_markers_file = 'path/to/aruco/markers.mat';
+% aruco_real_sides = [side_aruco1 side_aruco2 ...]; % [cm]
+
+% img_source = ...; % webcam(1) or 'path/to/image'
+
+
+%% DO NOT CHANGE BELOW
+% -------------------------------------------------------------------------
 
 run(config_file);
 load(aruco_markers_file);
